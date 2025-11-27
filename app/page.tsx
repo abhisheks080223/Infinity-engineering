387415fa867e22f3939a700c46f17507830f130b
abhishek080223 <abhishek004.cse21@kipm.edu.in>
2025-11-27 12:16:49 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import { HiMenu, HiX } from "react-icons/hi";
 
 export default function CompanyProfile() {
   const [open, setOpen] = useState(false);
@@ -17,9 +16,6 @@ export default function CompanyProfile() {
 
   return (
     <div className="w-full min-h-screen bg-white font-sans relative">
-      {/* ================================
-          TOP BANNER (FULL LOGO)
-      =================================*/}
       <div className="relative w-full h-[40vh]  flex items-center justify-center overflow-hidden">
         {/* FULLSCREEN LOGO */}
         <img
@@ -36,9 +32,7 @@ export default function CompanyProfile() {
           <button
             onClick={() => setOpen(!open)}
             className="bg-white/80 p-2 rounded-md shadow-md"
-          >
-            {open ? <HiX size={30} /> : <HiMenu size={30} />}
-          </button>
+          ></button>
         </div>
 
         {/* Desktop Menu */}
